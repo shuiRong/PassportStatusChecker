133b8e18fdf314333d8b836cadcf43cf3f8d63b7
林水溶 <shuirong1997@icloud.com>
2022-05-16 14:57:52 +0800

    refactor: 使用env读取配置；

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/shuiRong/PassportStatusChecker
+
+go 1.18
+
+require (
+	github.com/go-rod/rod v0.106.6
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
+)
+
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/gson v0.7.1 // indirect
+	github.com/ysmood/leakless v0.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/smtp"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
+	"github.com/jordan-wright/email"
+)
+
+func sendMail(content string) {
+	println("准备发送邮件...")
+	from := os.Getenv("FROM")
+	to := os.Getenv("TO")
+	passcode := os.Getenv("PASSWORD_CODE")
+	smtpAddress := os.Getenv("SMTP")
+	if smtpAddress == "" {
+		smtpAddress = "smtp.qq.com"
+	}
+	smtpPort := os.Getenv("SMTP_PORT")
+	if smtpPort == "" {
+		smtpPort = "25"
+	}
+
+	e := email.NewEmail()
+	//设置发送方的邮箱
+	e.From = from
+	// 设置接收方的邮箱
+	e.To = []string{to}
+	e.Subject = content
+	//设置文件发送的内容
+	e.Text = []byte(content)
+	//设置服务器相关的配置
+	err := e.Send(smtpAddress+":"+smtpPort, smtp.PlainAuth("", from, passcode, smtpAddress))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func regStringVar(p *string, name string, value string, usage string) {
+	if flag.Lookup(name) == nil {
+		flag.StringVar(p, name, value, usage)
+	}
+}
+
+var id, from, to, passcode, status, smtpAddress, smtpPort string
+
+func loadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	loadConfig()
+	page := rod.New().MustConnect().MustPage("")
+
+	router := page.HijackRequests()
+	// 阻止这个链接的加载，因为会判断权限，让页面跳转到广东统一身份认证平台
+	router.MustAdd("*/sq-utils.js", func(ctx *rod.Hijack) {
+		ctx.Response.Fail(proto.NetworkErrorReasonBlockedByClient)
+		return
+	})
+
+	go router.Run()
+
+	page.MustNavigate("https://crj.gdga.gd.gov.cn/gdfwzww/views/jdcx/jdcxjg.html").MustWaitLoad()
+
+	page.MustElement("#ZJHM").MustInput(os.Getenv("ID"))
+	page.MustElement("body div.gd-form-item.table-wsyymlpt button").MustClick()
+	statusDOM := page.MustElement("#query_search_table div.col-sm-2.states")
+	text := statusDOM.MustText()
+	println("状态：", text)
+	//  && text != status
+	if text != "" {
+		sendMail(text)
+	}
+}
